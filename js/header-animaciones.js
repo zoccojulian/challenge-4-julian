ec64dfbfb7d56a5208b770c6431783651ba25021
Julian Zocco <zoccojulian@gmail.com>
2022-04-13 20:29:38 -0300

    Pagina de login y funcionalidad de boton hacia esa página. Funcionalidad boton banner

--- js/header-animaciones.js
+++ js/header-animaciones.js
@@ -34,3 +34,15 @@ window.addEventListener("scroll", () =>{
 botonBusqueda.addEventListener("click", () => {
     barraBusqueda.classList.toggle("header__input-fuera")
 });
+
+
+
+/*Movimiento boton banner hacia sección predeterminada*/
+const botonBanner = document.querySelector(".banner__boton");
+    botonBanner.addEventListener("click", (evento) => {
+        evento.preventDefault();
+        let link = evento.currentTarget.getAttribute('href');
+        document.querySelector(link).scrollIntoView({
+            behavior: 'smooth'
+          });
+    });
